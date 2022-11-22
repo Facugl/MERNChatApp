ce31a2d5f9815e90658921923b7cea7fc0e44b69
Facundo Luna <facundolunaok@gmail.com>
2022-11-22 16:37:52 -0300

    add CORS

--- server/server.js
+++ server/server.js
@@ -17,6 +17,15 @@ app.use(express.json()); // to accept JSON Data
 //   res.send("API is Running Successfully");
 // });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
